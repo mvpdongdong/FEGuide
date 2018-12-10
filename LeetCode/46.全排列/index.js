2b7ea9455ad71a1ede7d27acfd1c90a6a1b55a42
申冬 <dongshen211299@sohu-inc.com>
2018-12-10 19:04:37 +0800

    全排列算法改进

--- LeetCode/46.全排列/index.js
+++ LeetCode/46.全排列/index.js
@@ -10,16 +10,16 @@ var permute = function (nums) {
         arr[j] = tmp;
     }
     const DFS = function(arr, len, index) {
-    if (index === len) {
-        result.push(arr.slice())
-        return
-    }
+        if (index === len) {
+            result.push(arr.slice())
+            return
+        }
 
-    for (let i = index; i < len; i++) {
-        swap(arr, index, i)
-        DFS(arr, len, index + 1)
-        swap(arr, index, i);
-    }
+        for (let i = index; i < len; i++) {
+            swap(arr, index, i)
+            DFS(arr, len, index + 1)
+            swap(arr, index, i);
+        }
     }
 
     DFS(nums, nums.length, 0)
